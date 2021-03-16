7dada423db0c22907feb9286ddea475cb2fc29ec
lonnen <chris.lonnen@gmail.com>
2021-03-16 01:32:54 -0700

    introducting htm
    preact has a precompile step but that feels like a lot to set up right now, so instead we're going to use the htm library to work with preact in a no build, no jsx manner.
    
    this will let us use Tagged Templates for rendering the UI
    
    eventually we may want to add babel and weplace all this with templates but let's defer that as far as we comfortably can.

--- src/content.js
+++ src/content.js
@@ -1,4 +1,8 @@
 const { ipcRenderer } = require('electron');
+const { h, Component, render } = require('preact');
+const htm = require('htm');
+
+const html = htm.bind(h);
 
 ipcRenderer.on('openFile', (event, file, content) => {
     console.log(file, content);
